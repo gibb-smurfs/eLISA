edcb4e15cc1c4ed2da1d98eeebe8b8303dddb18e
Matthias Osswald <m@osswald.li>
2019-10-20 16:15:02 +0200

    Improve query to search in title and content for each part.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,10 +13,24 @@ class SearchController extends Controller
 
     public function search($query)
     {
-        $query_array = explode("+", $query);
-
-        $search_results = Idea::leftJoin(DB::raw('(SELECT idea_id, AVG(rating) AS avg_rating FROM t_rating GROUP BY idea_id) a'), 'id', '=', 'idea_id')->select('id', 'name', 'title', 'content', 'created_at', 'updated_at', 'avg_rating')->orderBy('avg_rating', 'desc')->where('title', 'LIKE', '%' . $query_array[0] . '%')->get();
-
+        $where_string = $this->build_where_string($query);
+        $search_results = Idea::leftJoin(DB::raw('(SELECT idea_id, AVG(rating) AS avg_rating FROM t_rating GROUP BY idea_id) a'), 'id', '=', 'idea_id')->select('id', 'name', 'title', 'content', 'created_at', 'updated_at', 'avg_rating')->whereRaw($where_string)->get();
         return view('Home', ['ideas' => $search_results]);
+    }
+
+    private function build_where_string($query)
+    {
+        $query_parts = explode('+', $query);
+        $where_string = '';
+        $counter = 0;
+        foreach ($query_parts as $query_part)
+        {
+            if ($counter != 0) {
+                $where_string .= " OR ";
+            }
+            $where_string .= "title LIKE \"%$query_part%\" OR content LIKE \"%$query_part%\"";
+            $counter += 1;
+        }
+        return $where_string;
     }
 }
