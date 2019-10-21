bc9cb718c6ff904c808ed69c51ff9352b18b047d
Matteias Collet <matteias.collet@bluewin.ch>
2019-10-21 09:27:07 +0200

    noob

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,26 +10,24 @@ use Illuminate\Http\Request;
 class SearchController extends Controller
 {
     public function __construct()
-    {
-    }
+    { }
 
     public function search(Request $request)
     {
         //@TODO: sanitize inputs
 
-        $where_string = $this->build_where_string($request->input('search'));
-        $search_results = Idea::leftJoin(DB::raw('(SELECT idea_id, AVG(rating) AS avg_rating FROM t_rating GROUP BY idea_id) a'), 'id', '=', 'idea_id')->select('id', 'name', 'title', 'content', 'created_at', 'updated_at', 'avg_rating')->whereRaw($where_string)->get();
+        $where_string = $this->buildWhereString($request->input('query'));
+        $search_results = Idea::whereRaw($where_string)->leftJoin(DB::raw('(SELECT idea_id, ROUND(AVG(rating), 2) AS avg_rating FROM t_rating GROUP BY idea_id) a'), 'id', '=', 'idea_id')->select('id', 'name', 'title', 'content', 'created_at', 'updated_at', 'avg_rating')->get();
         return view('Home', ['ideas' => $search_results]);
     }
 
-    private function build_where_string($query)
+    private function buildWhereString($query)
     {
-        
-        $pattern = '/\s+/g';
+        $pattern = '/\s+/';
         $replacement = "%' OR '%";
-        
+
         $str = preg_replace($pattern, $replacement, trim($query));
-  
+
         return "title LIKE '%$str%' OR content LIKE '%$str%'";
     }
 }
