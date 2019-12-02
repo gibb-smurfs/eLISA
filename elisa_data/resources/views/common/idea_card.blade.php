bd8d72e9385c89b48d04738a4c2145a28dab7624
Matteias Collet <matteias.collet@bluewin.ch>
2019-12-02 12:49:37 +0100

    escape html in idea_card

--- elisa_data/resources/views/common/idea_card.blade.php
+++ elisa_data/resources/views/common/idea_card.blade.php
@@ -1,7 +1,7 @@
 <div class="card mb-3 p-1">
     <div class="card-body">
         <h5 class="card-title">{{$avg_rating}} <a href="/idea/{{$id}}">{{$card_title}}</a></h5>
-        <p class="card-text">{!! \Illuminate\Support\Str::words($card_content, 30,'....')  !!}</p>
+        <p class="card-text">{{ \Illuminate\Support\Str::words($card_content, 30,'....')  }}</p>
         <p class="small mb-0">Posted on {{$created_at}} by {{$author}}</p>
     </div>
 </div>
